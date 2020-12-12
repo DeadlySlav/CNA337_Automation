60fa2748f465750941890512999b9210c8373cee
DeadlySlav <65268111+DeadlySlav@users.noreply.github.com>
2020-12-11 19:50:29 -0800

    Update Server.py
    SSH code added

--- Server.py
+++ Server.py
@@ -15,3 +15,18 @@ class Server:
         # TODO - Use os module to ping the server
         server = os.system("ping -n 4 " + self.server_ip)
         return self.server_ip
+
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect('3.23.115.46', port=22, username='ec2-user', password='', key_filename=r'C:\Users\Eric\.ssh\id_rsa')
+
+stdin, stdout, stderr = ssh.exec_command('sudo yum update; sudo yum upgrade')
+stdin.flush()
+data = stdout.read().splitlines()
+for line in data:
+    print(line)
+
+ssh.close()
